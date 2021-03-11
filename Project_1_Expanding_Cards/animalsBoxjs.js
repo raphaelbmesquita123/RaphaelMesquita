0712170feaf9b7725c08f0cb7a032f4330ef193d
Raphael Mesquita <raphaelbmesquita@gmail.com>
2021-03-11 20:43:36 +0000

    Raphael

--- Project_1_Expanding_Cards/animalsBoxjs.js
+++ Project_1_Expanding_Cards/animalsBoxjs.js
@@ -1,14 +1,14 @@
-const panels = document.querySelectorAll(".panel")
+const panels = document.querySelectorAll(".panelPanel")
 
 panels.forEach((panel) => {
     panel.addEventListener('click', () => {+
         removeActiveClasses()
-        panel.classList.add('active')
+        panel.classList.add('activePanel')
     })
 })
 
 function removeActiveClasses() {
     panels.forEach( panel => {
-        panel.classList.remove('active')
+        panel.classList.remove('activePanel')
     })
 }
